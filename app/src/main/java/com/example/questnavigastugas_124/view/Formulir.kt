1284fecb0c0918a6634164c470f6be5aa0c68783
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-06 17:31:37 +0700

    tambahkan HorizontalDivider dan grup RadioButton untuk pilihan jenis kelamin

--- app/src/main/java/com/example/questnavigastugas_124/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_124/view/Formulir.kt
@@ -59,3 +59,19 @@ fun FormIsian(
                 onValueChange = {},
             )
         }
+        HorizontalDivider(modifier = Modifier
+            .padding(all = 20.dp)
+            .width(width = 250.dp), thickness = Thickness, color =
+        Color.Red)
+        Row{
+            jenisK.forEach {
+                    item->
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = false,
+                        onClick = {item}
+                    )
+                    Text(text = item)
+                }
+            }
+        }
