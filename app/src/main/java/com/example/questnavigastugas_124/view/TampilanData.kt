729e13c04112f193bd5b985ed4858539a547d3a0
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-11-06 17:41:04 +0700

    tambahkan tombol Back dengan Spacer serta stub fungsi stringResource

--- app/src/main/java/com/example/questnavigastugas_124/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigastugas_124/view/TampilanData.kt
@@ -80,7 +80,21 @@ fun TampilanData(
                     HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
             }
+            Spacer(modifier = Modifier.height(height = 10.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.back))
+            }
         }
     }
 }
+
+
+fun stringResource(id: String): String {
+    TODO("Not yet implemented")
+}
+
+
 
